906fbe1c23ce612e2273274be760ef8a3f9a7919
Andreas Marienborg <andreas.marienborg@gmail.com>
2007-07-20 07:45:11 +0000

    r15756@andreas-marienborgs-computer:  andreas | 2007-07-20 09:43:51 +0200  version 0.0103. Added missing build-req

--- lib/Catalyst/Action/RenderView/ErrorHandler.pm
+++ lib/Catalyst/Action/RenderView/ErrorHandler.pm
@@ -3,7 +3,7 @@ package Catalyst::Action::RenderView::ErrorHandler;
 use warnings;
 use strict;
 use Carp;
-our $VERSION = '0.0102';
+our $VERSION = '0.0103';
 
 use Class::Inspector;
 
@@ -26,10 +26,6 @@ sub execute {
     my $rv = $self->NEXT::execute(@_);
     return 1 unless (scalar(@{ $c->error }) or $c->res->status =~ /^4\d\d/);
     return 1 if ($c->debug);
-#    $c->log->debug('We are handeling this request');
-#    $c->log->debug($c->debug ? "   debug" : "no debug");
-#    $c->log->debug(scalar(@{ $c->error }) ? "   error" : "no error");
-#    $c->log->debug($c->res->status =~ /^4\d\d$/ ? "   status" : "no status");
     $self->actions({});
     $self->handlers([]);
     $self->_parse_config($c);
